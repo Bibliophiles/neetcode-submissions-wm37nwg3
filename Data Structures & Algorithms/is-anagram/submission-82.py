4d55ac71a39de283a4858cf96ee3025bf69bae1d
dennisamematekpor ANANE <108346201+Bibliophiles@users.noreply.github.com>
2026-05-27 12:50:38 +0200

    Add: is-anagram - submission-82

---
+++ Data Structures & Algorithms/is-anagram/submission-82.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(t) != len(s):
+            return False
+
+        char_count = [0] * 26
+        for i in range(len(s)):
+            char_count[ord(s[i]) - ord('a')] += 1
+            char_count[ord(t[i]) - ord('a')] -= 1
+
+        for val in char_count:
+            if val != 0:
+                return False 
+        
+        return True
